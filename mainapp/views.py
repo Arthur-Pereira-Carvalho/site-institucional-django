134e3f1f588d1d54ad8eefc402fe7ad4544da3b1
Arthur <carvalhopereiraarthur0401@gmail.com>
2026-04-08 14:01:42 -0300

    Funcionalidade: inicializar a estrutura do projeto Django com visualizações, modelos e rotas principais.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def servicos(request):
+    return render(request, 'servicos.html')
+
+def contato(request):
+    return render(request, 'contato.html')
